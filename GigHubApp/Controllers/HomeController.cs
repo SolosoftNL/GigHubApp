ddcdf61c005d54cf403e7182c28404ec86c37023
Ron <no@one.yet>
2016-06-17 16:46:04 +0200

    asfdsadf

--- GigHubApp/Controllers/HomeController.cs
+++ GigHubApp/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace GigHubApp.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return View(); 
         }
 
         public ActionResult Contact()
