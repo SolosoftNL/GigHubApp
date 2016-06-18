2721e74ad2df5f92fbb9652212895d11c4fba0c7
SolosoftNL <solosoft.nl@gmail.com>
2016-06-18 11:19:09 +0200

    models aangemaakt, migration toegevoegd

---
+++ GigHubApp/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigHubApp.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
