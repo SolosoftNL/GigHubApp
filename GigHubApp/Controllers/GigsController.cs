973f4ccda6a56279448c2f80c2f0071a8c1d463b
SolosoftNL <solosoft.nl@gmail.com>
2016-06-20 15:36:06 +0200

    Gigs form added

---
+++ GigHubApp/Controllers/GigsController.cs
@@ -0,0 +1,30 @@
+﻿using GigHubApp.Models;
+using GigHubApp.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHubApp.Controllers
+{
+    public class GigsController : Controller
+    {
+
+        private ApplicationDbContext _dbContext;
+
+        public GigsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var genre = new GigFormViewModel
+            {
+                Genres = _dbContext.Genres.ToList()
+            };
+
+            return View(genre);
+        }
+    }
+}
---
+++ GigHubApp/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GigHubApp.Models;
+using System.Collections.Generic;
+namespace GigHubApp.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Vanue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
